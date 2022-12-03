18a398c84ac9af07be83e71bdf24fc7bf093d506
Jonathan <jonathan.gualli@epn.edu.ec>
2022-12-03 17:45:08 -0500

    Segundo comit al main desde mi pc

--- src/ultimaprueba/UltimaPrueba.java
+++ src/ultimaprueba/UltimaPrueba.java
@@ -5,6 +5,7 @@ public class UltimaPrueba {
 
     public static void main(String[] args) {
         System.out.println("Prmer commit");
+        System.out.println("segundo commit");
     }
     
 }
